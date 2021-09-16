e8d706527ef5f8d24c7cd8113cb8f30558cd6070
steff880 <stefan.d.yordanov@gmail.com>
2021-09-16 14:41:09 +0000

    Added forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -68,7 +68,7 @@ class OrderLineItem(models.Model):
         Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True)  # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    leneitem_total = models.DecimalField(
+    lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
@@ -76,7 +76,7 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.leneitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
